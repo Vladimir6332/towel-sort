d6a1950fb561059a18eb183563135693e199e544
Vladimir6332 <vladimir6332@gmail.com>
2020-10-21 15:24:36 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let res = [];
+    if (!matrix || matrix.length === 0) return res;
+    const minMax = (a, b) => a - b;
+    const maxMin = (a, b) => b - a;
+    res = matrix.reduce((acc, val, index) => {
+        (index + 1) % 2
+            ? (acc = acc.concat(val.sort(minMax)))
+            : (acc = acc.concat(val.sort(maxMin)));
+        return acc;
+    }, []);
+    return res;
+};
